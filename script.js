4d5c026948f0dee73d9e4450c31187721a3216ad
Alexssandro Ferreira da Silva <alexssandro.ferreira@ifsp.edu.br>
2026-04-12 12:58:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -216,7 +216,8 @@ function exibirTotalPorMotorista() {
             };
         }
         // Adiciona a descrição limpa do veículo
-        agrupado[chave].veiculos.push(${v.tipo}: ${v.tipo}: <strong>${v.modelo}</strong> (<strong>${v.placa}</strong>) - <strong>${v.cor}</strong>);
+        // O jeito certo de "empurrar" o texto com as tags para a lista:
+        agrupado[chave].veiculos.push(`${v.tipo}: <strong>${v.modelo}</strong> (<strong>${v.placa}</strong>) - <strong>${v.cor}</strong>`);
     });
 
     // 2. Transformar em lista e ordenar (quem tem mais veículos aparece primeiro)
